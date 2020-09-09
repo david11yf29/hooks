3d09b59f442beea5a560d3de331414c033f0a22a
david11yf29 <david11yf29@gmail.com>
2020-09-10 07:47:02 +0800

    updated

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,11 +7,39 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-hooks-8a009.firebaseio.com/ingredients.json')
+      .then(res => res.json())
+      .then(resData => {
+        const loadedIngredients = [];
+        for (const key in resData) {
+          // 創一個 array 把收到 data 放進去
+          loadedIngredients.push({
+            id: key,
+            title: resData[key].title,
+            amount: resData[key].amount
+          })
+        }
+        // 再把這個 array 丟到 setState
+        setUserIngredients(loadedIngredients);
+      })
+  }, [])
+
+  
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch('https://react-hooks-8a009.firebaseio.com/ingredients.json', {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: {"Content-Type": "application/json"}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: Math.random().toString(), ...ingredient }
+      ]);
+    });
   };
 
   return (
