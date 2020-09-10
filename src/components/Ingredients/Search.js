a53abdb08707b70539390be2dc35ff2e14a05b44
david11yf29 <david11yf29@gmail.com>
2020-09-10 14:54:00 +0800

    updated

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [enteredFilter, setEnteredFilter] = useState([]);
+
+  useEffect(() => {
+    
+  }, [enteredFilter])
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input 
+            type="text" 
+            value={enteredFilter} 
+            onChange={event => {
+              // State 裡面為 我們輸入的內容
+              setEnteredFilter(event.target.value)
+            }} />
         </div>
       </Card>
     </section>
